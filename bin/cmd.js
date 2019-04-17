c86dc9f1a03e600c04914c087e2c129ff37e44b5
Dencer <tdaddon@163.com>
2019-04-17 14:31:46 +0800

    update bin

---
+++ bin/cmd.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+/**
+ * @File   : cmd.js
+ * @Author : Dencer (tdaddon@163.com)
+ * @Link   : https://dengsir.github.io
+ * @Date   : 4/17/2019, 2:27:13 PM
+ */
+
+require('../dist/index');
